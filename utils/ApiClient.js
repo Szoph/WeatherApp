98acfade0225b95b5c31f90b3bcb0f11fda5c18f
Sophie Woodcock <szoph101@hotmail.co.uk>
2024-04-24 16:49:10 +0100

    fix 2

--- utils/ApiClient.js
+++ utils/ApiClient.js
@@ -2,9 +2,9 @@ import { GET } from '@/app/api/geocoding/route';
 
 export class ApiClient {
 
-    async getRequest(location) {
+    async getRequest(searchText) {
         try {
-            const response = await GET(location);
+            const response = await GET(searchText);
             return response ? response : null;
         } catch (error) {
             console.error(error);
